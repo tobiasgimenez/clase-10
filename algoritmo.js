85927ba643b3d51c87146071f193b7da9f0c4ea1
tobias gimenez~ <tobiasgimenezchirino@gmail.com~>
2022-04-28 11:12:49 -0300

    agregue algoritmo e index

---
+++ algoritmo.js
@@ -0,0 +1,56 @@
+// Inicializo variables de mi algoritmo
+
+var cantBananas = 2;
+var string = "Tapar la licuadora";
+var cantHielos = 8;
+var seParoLaLicuadora= true;
+var grAzucar = 50.01
+// Finalice las variables de  mi algoritmo
+
+// Problema 1
+var manzanasMaria = 5;
+var manzanasPedro = 8;
+var manzanasSusana = 9;
+var totalManzanas = manzanasMaria + manzanasPedro + manzanasSusana;
+
+// Problema 2 
+var magdalenaJuan = 28;
+var magdanelaAmigos = 12;
+var cantManzanas = 0;
+
+// Problema 3
+var preciohamburguesa = 35;
+var preciorefresco = 18;
+var totaldinero = preciohamburguesa + preciorefresco;
+
+// Problema 4
+var cantGalletas = 5;
+var cantCajas = 4;
+var totalGalletas = cantgalletas * cantCajas;
+
+// Problema 5
+var distanciaToledo = 2.6; //Kilometros
+var distanciaCasaTio = 691; //Metros
+var distanciaTotalMts = (distanciaToledo *100 ) + distanciaCasaTio;
+var distanciaTotalMilimetros = distanciaTotalMts * 100;
+
+document.write(`
+
+    <h4> -Agarrar la licuadora, 1 vaso, un cuchillo y una tabla </h4> 
+    <h4> -Apartar 2 bananas, medio litro de leche, 50 gr de azucar y una cubetera de hielo </h4> 
+    <h4> -Agarrar la licuadora y conectarla al enchufe </h4> 
+    <h4> -Colocar 8 hielos adentro de la licuadora </h4> 
+    <h4> -Agarrar el cuchillo y la tabla; cortar las 2 bananas </h4> 
+    <h4> -Poner los trozos de banana en la licuadora </h4> 
+    <h4> -Verter los 50gr de azucar dentro de la licuadora </h4> 
+    <h4> -Volcar el medio litro de leche en la licuadora </h4> 
+    <h4> -Tapar la licuadora </h4>  
+    <h4> -Prender la licuadora </h4> 
+    <h4> -Licuar 2 minutos la mezcla </h4> 
+    <h4> -Parar la licuadora </h4>  
+    <h4> -Sacar la tapa </h4> 
+    <h4> -Agarrar el vaso </h4>  
+    <h4> -Verter la mezcla dentro del vaso </h4> 
+    <h4> -Desenchufar la licuadora  </h4>  
+
+`)
